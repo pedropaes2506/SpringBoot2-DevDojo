8dbcfad10c6448971b2a90ea2ccdc7ecaab2d6e9
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-08-06 18:44:23 -0300

    Hot Swap

--- main/java/springboot2/controller/AnimeController.java
+++ main/java/springboot2/controller/AnimeController.java
@@ -1,11 +1,9 @@
 package main.java.springboot2.controller;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import main.java.springboot2.domain.Anime;
 import main.java.springboot2.util.DateUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +22,6 @@ public class AnimeController {
     @GetMapping(path = "list")
     public List<Anime> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return List.of(new Anime("DBZ"), new Anime("Berzerk"));
+        return List.of(new Anime("Boku no Hero"), new Anime("Berzerk"));
     }
 }
