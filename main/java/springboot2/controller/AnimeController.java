41b4f6c41bd2deed37e4deaa5f4ff27b019436a7
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-08-06 18:39:01 -0300

    @Component | @Autowired | @SpringBootApplication

---
+++ main/java/springboot2/controller/AnimeController.java
@@ -0,0 +1,29 @@
+package main.java.springboot2.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import main.java.springboot2.domain.Anime;
+import main.java.springboot2.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@RequiredArgsConstructor
+public class AnimeController {
+    private final DateUtil dateUtil;
+
+    // localhost:8080/anime/list
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("Berzerk"));
+    }
+}
