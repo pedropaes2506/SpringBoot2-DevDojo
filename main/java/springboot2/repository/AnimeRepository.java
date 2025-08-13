316470978a11e86ecd1acf248d381f243553fb74
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-08-13 17:46:02 -0300

    Request Params

--- main/java/springboot2/repository/AnimeRepository.java
+++ main/java/springboot2/repository/AnimeRepository.java
@@ -3,6 +3,8 @@ package main.java.springboot2.repository;
 import main.java.springboot2.domain.Anime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnimeRepository extends JpaRepository<Anime, Long> {
+import java.util.List;
 
+public interface AnimeRepository extends JpaRepository<Anime, Long> {
+    List<Anime> findByName(String name);
 }
