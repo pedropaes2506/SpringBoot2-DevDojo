a3b1befe4e06e8ea3bf25a13eb2e2fee4f7c4ac0
Pedro Paes Rocha <pedrokarocha530@gmail.com>
2025-08-21 18:31:22 -0300

    RestTemplate | exchange

--- main/java/springboot2/client/SpringClient.java
+++ main/java/springboot2/client/SpringClient.java
@@ -2,8 +2,13 @@ package main.java.springboot2.client;
 
 import lombok.extern.log4j.Log4j2;
 import main.java.springboot2.domain.Anime;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Log4j2
 public class SpringClient {
@@ -13,5 +18,16 @@ public class SpringClient {
 
         Anime object = new RestTemplate().getForObject("http://localhost:8080/animes/2", Anime.class);
         log.info(object);
+
+        Anime[] animes = new RestTemplate().getForObject("http://localhost:8080/animes/all", Anime[].class);
+        log.info(Arrays.toString(animes));
+
+        ResponseEntity<List<Anime>> exchange = new RestTemplate().exchange(
+                "http://localhost:8080/animes/all",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Anime>>() {}
+        );
+        log.info(exchange.getBody());
     }
 }
